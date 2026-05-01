86c4a37b876ec7ed8a5b42d5c3f7b3c4b0bd5c03
Lorenzo Stoakes <ljs@kernel.org>
2026-05-01 20:27:41 +0100

    mm: add simple dynamic array implementation
    This is to be used to supplement a maple tree that will track anon remaps
    for CoW context.
    
    It assumes that the caller takes care of write locking (ultimately the
    write operations will be those which hold the exclusive mmap write lock).
    
    It can have concurrent readers and writers, but mutations of the dynamic
    array can happen simultaneously.
    
    Unconvincingly wave my arms and say 'hey READ_ONCE() for 64-bit kernels
    really does have to be tear-free, and if we acquire/release load and store
    the dynamic array count we kinda probably are sort of ok'.
    
    Yes, this needs improvement.
    
    It also does a nasty O(n^2) insertion sort to keep the array unique,
    however that and it (being a small array) are least _vaguely_
    cacheline-friendly :)
    
    Overall, it's very iffy. But this is a proof of concept! :)
    
    Signed-off-by: Lorenzo Stoakes <ljs@kernel.org>

--- mm/cow_context.c
+++ mm/cow_context.c
@@ -12,6 +12,212 @@
 #include <linux/rmap.h>
 #include "internal.h"
 
+/* Simple dynamic array. */
+struct dynarray {
+	int nr, cap;
+	void *contents[] __counted_by(cap);
+};
+
+/* Dynamic array API. */
+
+struct dynarray *dynarray_dup(struct dynarray *arr, gfp_t gfp);
+struct dynarray *__dynarray_append(struct dynarray *arr, void *val, gfp_t gfp);
+struct dynarray *__dynarray_append_stack(struct dynarray *arr,
+					 void *val, gfp_t gfp);
+void dynarray_shrink_rcu(struct dynarray *arr);
+
+#define EMPTY_DYNARRAY ((struct dynarray) {})
+
+#define __DECLARE_DYNARRAY(_name, _nr, _cap, _val)			\
+	union {								\
+		struct {						\
+			int nr, cap;					\
+			void *contents[_cap];				\
+		};							\
+		struct dynarray arr;					\
+	} _name = {							\
+		.nr = _nr,						\
+		.cap = _cap,						\
+		.contents = { _val }					\
+	}
+
+#define DECLARE_DYNARRAY_SINGLE(_name, _val)			 \
+	__DECLARE_DYNARRAY(_name ## _arr, 1, 1, (void *)(_val)); \
+	struct dynarray *_name = &((_name ## _arr).arr)
+
+#define DECLARE_DYNARRAY(_name, _cap)				\
+	__DECLARE_DYNARRAY(_name ## _arr, /*nr=*/0, _cap,);	\
+	struct dynarray *_name = &((_name ## _arr).arr)
+
+#define DYNARRAY_DEFAULT_CAP 16
+
+
+#define __dynarray_get_t(type_or_val, _idx, _arr)		\
+	((typeof(type_or_val))READ_ONCE(_arr->contents[_idx]))
+
+#define __dynarray_for_each_reverse(_idx, _arr, _val)			  \
+	for (_idx = dynarray_nr(_arr) - 1;				  \
+	     _idx >= 0 && (_val = __dynarray_get_t(_val, _idx, _arr), 1); \
+	     _idx--)
+
+#define __dynarray_set(_idx, _arr, _val)			\
+	do {							\
+		WRITE_ONCE(_arr->contents[_idx], (void *)_val);	\
+	} while (0)
+
+static int __dynarray_nr(struct dynarray *arr)
+{
+	return READ_ONCE(arr->nr);
+}
+
+static int dynarray_nr(struct dynarray *arr)
+{
+	return smp_load_acquire(&arr->nr);
+}
+
+static void dynarray_set_nr(struct dynarray *arr, int new)
+{
+	smp_store_release(&arr->nr, new);
+}
+
+static int dynarray_cap(struct dynarray *arr)
+{
+	/* Will only be updated once RCU-assigned. */
+	return arr->cap;
+}
+
+static struct dynarray *__dynarray_dup(struct dynarray *arr, int cap, gfp_t gfp)
+{
+	const int nr = __dynarray_nr(arr);
+	struct dynarray *ret;
+
+	ret = kmalloc_flex(*arr, contents, cap, gfp);
+	if (!ret)
+		return NULL;
+
+	/* Just allocated, safe to assign. */
+	ret->nr = nr;
+	ret->cap = cap;
+
+	if (arr)
+		memcpy(ret->contents, arr->contents, sizeof(void *) * nr);
+	return ret;
+}
+
+struct dynarray *dynarray_dup(struct dynarray *arr, gfp_t gfp)
+{
+	return __dynarray_dup(arr, dynarray_cap(arr), gfp);
+}
+
+/*
+ * TODO: Insertion sort into a sorted array, O(n^2) aggregate but cache
+ *       friendly. We can probably do better though :)
+ */
+static void __dynarray_append_value(struct dynarray *arr, void *new_val)
+{
+	int i, insert_idx;
+	unsigned long val;
+	unsigned long new_val_raw = (unsigned long)new_val;
+
+	VM_WARN_ON_ONCE(arr->nr == arr->cap);
+
+	/* Check if dupe, find insert point... */
+	__dynarray_for_each_reverse(i, arr, val) {
+		if (val == new_val_raw)
+			return;
+		if (val < new_val_raw)
+			break;
+	}
+	insert_idx = i + 1;
+
+	/* ...Move everything up... */
+	__dynarray_for_each_reverse(i, arr, val) {
+		if (i <= insert_idx)
+			break;
+		__dynarray_set(i + 1, arr, val);
+	}
+	/* ...and insert. */
+	__dynarray_set(insert_idx, arr, new_val);
+	/* We assume we are the exclusive writer, so no risk of a tear. */
+	dynarray_set_nr(arr, __dynarray_nr(arr) + 1);
+}
+
+static struct dynarray *__dynarray_expand(struct dynarray *arr,
+					  bool from_stack, gfp_t gfp)
+{
+	const int new_cap = 2 * dynarray_cap(arr);
+	struct dynarray *ret;
+	size_t new_size;
+
+	VM_WARN_ON_ONCE(__dynarray_nr(arr) != dynarray_cap(arr));
+
+	if (from_stack)
+		return __dynarray_dup(arr, new_cap, gfp);
+
+	new_size = struct_size(arr, contents, new_cap);
+	ret = krealloc(arr, new_size, gfp);
+	if (!ret)
+		return ret;
+
+	/*
+	 * Other fields copied over, only need to update capacity. Safe as
+	 * nothing references this yet.
+	 */
+	ret->cap = new_cap;
+	return ret;
+}
+
+static bool __dynarray_needs_expand(struct dynarray *arr)
+{
+	VM_WARN_ON_ONCE(arr->nr > arr->cap);
+
+	return dynarray_cap(arr) == __dynarray_nr(arr);
+}
+
+static struct dynarray *__dynarray_maybe_expand(struct dynarray *arr,
+						bool from_stack, gfp_t gfp)
+{
+	if (__dynarray_needs_expand(arr))
+		return __dynarray_expand(arr, from_stack, gfp);
+	return arr;
+}
+
+struct dynarray *__dynarray_append(struct dynarray *arr, void *val, gfp_t gfp)
+{
+	struct dynarray *new_arr;
+
+	new_arr = __dynarray_maybe_expand(arr, /*from_stack=*/false, gfp);
+	__dynarray_append_value(new_arr, val);
+	return new_arr;
+}
+
+struct dynarray *__dynarray_append_stack(struct dynarray *arr,
+					 void *val, gfp_t gfp)
+{
+	struct dynarray *new_arr;
+
+	new_arr = __dynarray_maybe_expand(arr, /*from_stack=*/true, gfp);
+	__dynarray_append_value(new_arr, val);
+	return new_arr;
+}
+
+static void dynarray_free_rcu(struct dynarray *arr)
+{
+	/* We're being accessed by RCU readers. */
+	kfree_rcu_mightsleep(arr);
+}
+
+void dynarray_shrink_rcu(struct dynarray *arr)
+{
+	const int nr = __dynarray_nr(arr);
+
+	/* We are the exclusive writer so no chance of a tear. */
+	if (nr == 1) {
+		dynarray_set_nr(arr, 0);
+		dynarray_free_rcu(arr);
+	}
+}
+
 static struct cow_context *delete_child_from_parent(struct cow_context *context)
 {
 	/*
